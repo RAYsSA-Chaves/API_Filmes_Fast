c5654666ab493d3d5ab9ffcb1c779c060e53ee14
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-11-10 10:59:11 -0300

    Update security.py

--- Backend/core/security.py
+++ Backend/core/security.py
@@ -7,12 +7,20 @@ from pwdlib import PasswordHash
 
 pwd_context = PasswordHash.recommended()  # ele decide sozinho como hashear
 
-import secrets  # gera "segredos" 
+import secrets  # gera "segredos" / números aleatórios e secretos
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.deps import get_session
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer  # extrai token Bearer do header HTTP
+
 
 # Variáveis para o token
 SECRET_KEY = secrets.token.urlsafe(32)
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')  # indica o endpoint onde o cliente pode obter o token (documentação)
 
 
 # Função para hashear uma senha
@@ -32,3 +40,8 @@ def create_access_token(data: dict):
     to_encode.update({'exp': expire})
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+    
+# Função para verificar autorização
+def get_current_user(db: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)):
+    ...
